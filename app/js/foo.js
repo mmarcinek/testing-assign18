506582d709582da642d2a95490a368a013c0424a
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-10 16:10:09 -0400

    Finishes Foo tests

--- app/js/foo.js
+++ app/js/foo.js
@@ -1,13 +1,31 @@
 window.Foo = (function() {
 
   function Foo() {
+    this.plusOne = function (num){
+      return num + 1;
+    };
 
-  }
+  this.sortArray = function (unsorted){
+    var sorted = unsorted.slice().sort();
+    return sorted;
+    };
 
-  Foo.prototype = {
+  this.hasValue = function (arr, value) {
+    return (arr.indexOf(value) >= 0);
+    };
 
-  };
+  // Foo.prototype = {
+
+  // };
+
+    Foo.greet = function (name){
+      return 'Hello, ' + name + '!';
+    };
+
+ };
 
   return Foo;
+
+
 
 })();
